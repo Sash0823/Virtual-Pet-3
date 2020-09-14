4efaf3f403d380ac40d58d2f31642153dd9326aa
Sash0823 <67402337+Sash0823@users.noreply.github.com>
2020-09-13 21:04:55 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,91 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var box1, box2,box3;
+var box1;
+
+var dog;
+this.image = loadImage("sprites/cartoon-dog-names.png");
+
+var gameState = "onSling";
+
+var score = 0;
+
+function preload() {
+}
+
+function setup(){
+    var canvas = createCanvas(1200,400);
+    engine = Engine.create();
+    world = engine.world;
+
+
+    ground = new Ground(600,height,1200,20);
+
+    box1 = new Box(700,320,70,70);
+    box2 = new Box(920,320,70,70);
+    box3 = new Box(700,240,70,70);
+    ball = new Ball();
+}
+
+function draw(){
+    if(backgroundImg)
+        background(backgroundImg);
+    
+        noStroke();
+        textSize(35)
+        fill("white")
+        text("Score  " + score, width-300, 50)
+    
+    Engine.update(engine);
+    //strokeWeight(4);
+    box1.display();
+    box2.display();
+    box3.display();
+    ball1.display(); 
+}
+
+function mouseDragged(){
+    //if (gameState!=="launched"){
+        Matter.Body.setPosition(ball.body, {x: mouseX , y: mouseY});
+    //}
+}
+
+
+function mouseReleased(){
+    ball.fly();
+    gameState = "launched";
+}
+
+function keyPressed(){
+    if(keyCode === 32){
+        ball.trajectory = [];
+        Matter.Body.setPosition(ball1.body, {x: 200 , y: 50});
+       slingshot.attach(ball.body);
+    }
+}
+
+async function getBackgroundImg(){
+    var response = await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata");
+    var responseJSON = await response.json();
+
+    var datetime = responseJSON.datetime;
+    var hour = datetime.slice(11,13);
+    
+    if(hour>=0600 && hour<=1900){
+        bg = "sprites/bg1.png";
+    }
+    else{
+        bg = "sprites/bg2.jpg";
+    }
+
+    backgroundImg = loadImage(bg);
+    console.log(backgroundImg);
+}
+
+
+
+
